e4d218c7bb88671305f1afe49fc6ad0f902d41db
KaraVictor <kva@tomsk.cc>
2026-02-08 17:57:45 +0300

    Update planner.cpp

--- components/planner/planner.cpp
+++ components/planner/planner.cpp
@@ -1,11 +1,24 @@
+#include <iostream>
+
 #include <components/optimizer/optimizer.hpp>
 #include "planner.hpp"
 
 namespace components::planner {
 
-    logical_plan::node_ptr planner_t::create_plan(std::pmr::memory_resource* resource, logical_plan::node_ptr node) {
-        components::optimizer::optimizer_t optimizer{};
-        return optimizer.optimize(resource, std::move(node));
-    }
+logical_plan::node_ptr planner_t::create_plan(
+    std::pmr::memory_resource* resource,
+    logical_plan::node_ptr node) {
 
+    std::cout << "\n================ LOGICAL PLAN BEFORE OPTIMIZATION ================\n";
+    std::cout << node->to_string() << std::endl;
+    std::cout << "==================================================================\n";
+
+    components::optimizer::optimizer_t optimizer{};
+    auto optimized = optimizer.optimize(resource, std::move(node));
+
+    std::cout << "\n================ LOGICAL PLAN AFTER OPTIMIZATION =================\n";
+    std::cout << optimized->to_string() << std::endl;
+    std::cout << "==================================================================\n";
+
+    return optimized;
 } // namespace components::planner
