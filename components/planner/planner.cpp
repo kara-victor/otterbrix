9c9be36bd22a3fd81aef9241f307eba337d4fa4a
KaraVictor <kva@tomsk.cc>
2026-02-08 19:31:04 +0300

    bug fixes

--- components/planner/planner.cpp
+++ components/planner/planner.cpp
@@ -5,20 +5,19 @@
 
 namespace components::planner {
 
-logical_plan::node_ptr planner_t::create_plan(
-    std::pmr::memory_resource* resource,
-    logical_plan::node_ptr node) {
+    logical_plan::node_ptr planner_t::create_plan(std::pmr::memory_resource* resource, logical_plan::node_ptr node) {
+        std::cout << "\n================ LOGICAL PLAN BEFORE OPTIMIZATION ================\n";
+        std::cout << node->to_string() << std::endl;
+        std::cout << "==================================================================\n";
 
-    std::cout << "\n================ LOGICAL PLAN BEFORE OPTIMIZATION ================\n";
-    std::cout << node->to_string() << std::endl;
-    std::cout << "==================================================================\n";
+        components::optimizer::optimizer_t optimizer{};
+        auto optimized = optimizer.optimize(resource, std::move(node));
 
-    components::optimizer::optimizer_t optimizer{};
-    auto optimized = optimizer.optimize(resource, std::move(node));
+        std::cout << "\n================ LOGICAL PLAN AFTER OPTIMIZATION =================\n";
+        std::cout << optimized->to_string() << std::endl;
+        std::cout << "==================================================================\n";
 
-    std::cout << "\n================ LOGICAL PLAN AFTER OPTIMIZATION =================\n";
-    std::cout << optimized->to_string() << std::endl;
-    std::cout << "==================================================================\n";
+        return optimized;
+    }
 
-    return optimized;
 } // namespace components::planner
